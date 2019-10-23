16ddb0ed2a0b188a92e31f4bc905aa1e37d870be
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-22 19:03:39 -0700

    link to home.handlebars is working

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-const express = require("express")
-const exphbs = require("express-handlebars")
-const app = express()
- 
+var express = require("express");
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
 var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.get("/", function (req, res) {
-  res.render("home")
-})
- 
-app.listen(PORT,function(){
-    console.log("APP listening on http://localhost:" + PORT)
-})
+app.get('/', function (req, res, next) {
+  res.render('home');
+});
 
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
