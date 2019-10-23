47085e38e43c966262ab92bcfbc0ae912bc5826c
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-22 18:24:05 -0700

    server is up and running

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
  
-var PORT = process.argv || 3000;
+var PORT = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
