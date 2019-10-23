c98b74b0b0e3730b4977383c45a093bb377519c8
Roger Zavala <Rogeliozavala15@gmail.com>
2019-10-22 18:50:14 -0700

    push

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
+const express = require("express")
+const exphbs = require("express-handlebars")
 const app = express()
  
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", function (req, res) {
+  res.render("home")
 })
  
 app.listen(PORT,function(){
